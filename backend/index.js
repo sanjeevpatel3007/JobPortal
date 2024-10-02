26c72face780c8a714f5026f510038f763aeb156
Sanjeev Patel <153625737+sanjeevpatel3007@users.noreply.github.com>
2024-10-02 18:58:52 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-frontend-mu.vercel.app/',
-    // origin:' http://localhost:5173',
-   
+    origin:'*',
     credentials:true
 }
+ // origin:'https://job-portal-frontend-mu.vercel.app/',
+    // origin:' http://localhost:5173',
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
